8c708e51484a280185ee6a8db824d33a054cbbab
Domingos Santos <domsantos@gmail.com>
2015-09-14 20:14:31 -0300

    Classes anotadas

---
+++ lojavirtualauto/src/main/java/br/ufpa/tap2/dao/AutomovelDAO.java
@@ -0,0 +1,9 @@
+package br.ufpa.tap2.dao;
+
+import br.ufpa.tap2.model.Automovel;
+
+/**
+ * Created by domingossantos on 14/09/15.
+ */
+public class AutomovelDAO extends DAO<Automovel> {
+}
---
+++ lojavirtualauto/src/main/java/br/ufpa/tap2/dao/MarcaDAO.java
@@ -0,0 +1,9 @@
+package br.ufpa.tap2.dao;
+
+import br.ufpa.tap2.model.Marca;
+
+/**
+ * Created by domingossantos on 14/09/15.
+ */
+public class MarcaDAO extends DAO<Marca> {
+}
